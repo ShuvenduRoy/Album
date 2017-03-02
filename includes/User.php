ee568f0b6bb1b401221fc1b546309d5cc784527c
Bikash <bikash11roy@gmail.com>
2017-03-02 22:57:28 +0600

    Fully object oriented featured added

--- includes/User.php
+++ includes/User.php
@@ -11,6 +11,12 @@ require_once ('database.php');
 
 class User
 {
+    public $id;
+    public $username;
+    public $password;
+    public $first_name;
+    public $last_name;
+
     public static function find_all()
     {
         return self::find_by_sql("select * from users");
@@ -19,14 +25,46 @@ class User
     public static function find_by_id($id=0)
     {
         global $database;
-        $result_set = $database->query("select * from users where id={$id}");
-        $found = $database->fetch_array($result_set);
-        return $found;
+        $result_array = self::find_by_sql("select * from users where id={$id}");
+        return !empty($result_array) ? array_shift($result_array):false;
     }
 
-    public static function find_by_sql($sql=""){
+    public static function find_by_sql($sql="")
+    {
         global $database;
         $result_set = $database->query($sql);
-        return $result_set;
+        $object_array = array();
+
+        while ($row = $database->fetch_array($result_set)){
+            $object_array[] = self::instantiate($row);
+        }
+
+        return $object_array;
+    }
+
+    private static function instantiate($record){
+        $user               = new User();
+        /*$user->id           = $record['id'];
+        $user->username     = $record['username'];
+        $user->password     = $record['password'];
+        $user->first_name   = $record['first_name'];
+        $user->last_name    = $record['last_name'];*/
+
+        foreach ($record as $attribute=>$value){
+            if($user->has_attribute($attribute)){
+                $user->$attribute = $value;
+            }
+        }
+
+        return $user;
+    }
+
+    private function has_attribute($attribute){
+        $object_vars = get_object_vars($this);
+        return array_key_exists($attribute, $object_vars);
+    }
+
+    public function fullname(){
+        return $this->first_name." ".$this->last_name;
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once ("../includes/include.php");
 
-$record = User::find_by_id(1);
-echo $record['username'];
+$user = User::find_by_id(1);
+echo $user->fullname();
