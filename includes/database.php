e3d4c6cfb633aff7ef330ed6ff5562ed1aed41f3
Bikash <bikash11roy@gmail.com>
2017-03-02 13:43:17 +0600

    Database query system added

--- includes/database.php
+++ includes/database.php
@@ -2,8 +2,15 @@
 require_once ("config.php");
 
 class MySQLDatabase{
-    function open_conntection(){
-        $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+    private $connection;
+
+    function __construct()
+    {
+        $this->open_connection();
+    }
+
+    public function open_connection(){
+        $this->connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
         if(mysqli_errno()){
             die("Database connection failed: " .
                 mysqli_connect_error().
@@ -11,8 +18,34 @@ class MySQLDatabase{
             );
         }
     }
+
+    public function close_connection()
+    {
+        if(isset($this->connection)){
+            mysqli_close($this->connection);
+            unset($this->connection);
+        }
+    }
+
+    public function query($sql)
+    {
+        $result = mysqli_query($this->connection, $sql);
+
+        if(!$result){
+            die("Database connnection failed");
+        }
+
+        return $result;
+    }
+
+    public function mysql_prep($string)
+    {
+        $escaped_string = mysqli_real_escape_string($this->connection, $string);
+        return $escaped_string;
+    }
 }
 
-$batabase = new MySQLDatabase();
+$database = new MySQLDatabase();
+$db = & $database;
 
 ?>
